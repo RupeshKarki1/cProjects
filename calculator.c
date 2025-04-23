d94e5a9e5f51bfe930e078299adf2bc31a38e71e
SadN00dle <sadnoodle11@gmail.com>
2025-04-23 17:49:01 +0545

    userINput refactor, add subtract and exit function

--- calculator.c
+++ calculator.c
@@ -1,20 +1,57 @@
 #include <stdio.h>
 
 //global variables
-int num1;
-int num2;
+double userNum1;
+double userNum2;
 
+int numOneScan;
+int numTwoScan;
 
+char overFlowNum1;
+char overFlowNum2;
+
+int operationNum;
+char charOperationNum;
 //function for operations
 
 //add
-int add(num1, num2){
-    
+void add(double num1, double num2){   
+    double result = num1 + num2;
+    printf("Addition result: %lf", result);
 }
 
+void subtract(double num1, double num2){
+    double result = num1 - num2;
+    printf("Addition result: %lf", result);
+}
+
+void exitFunc(){
+
+}
+
+void userInput(){
+    printf("enter the operation to be performed: ");
+    int operation = scanf("%i", &charOperationNum);
 
 
-int main(){
+    printf("enter your first number: ");
+    numOneScan = scanf("%lf%c", &userNum1, &overFlowNum1);
+
+    printf("enter your second number: ");
+    numTwoScan = scanf("%lf%c", &userNum2, &overFlowNum2);
+
     
+}
+int main(){
+    userInput();
+    if (numOneScan != 2 || overFlowNum1 != '\n')
+    {
+        printf("please enter a valid number\n");
+    }
+    if (numTwoScan != 2 || overFlowNum2 != '\n')
+    {
+        printf("please enter a valid number\n");
+    }
+    add(userNum1, userNum2);
     return 0;
 }
