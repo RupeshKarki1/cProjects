b4f9d8b295d1aaa334883231344e4df0e794e53d
SadN00dle <sadnoodle11@gmail.com>
2025-04-24 14:30:26 +0545

    add userManagement proj and made userPrompt

---
+++ userManagement.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void userPrompt(){
+    printf("1.Register\n");
+    printf("2.login\n");
+    printf("3.exit\n");
+    printf("select an option: ");
+}
+
+int main(){
+    userPrompt();
+}
