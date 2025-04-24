1e1b361ff0e4029f4e9e7ebdec0409fe536883ad
SadN00dle <sadnoodle11@gmail.com>
2025-04-24 15:30:04 +0545

    add userPrompt functionality

--- userManagement.c
+++ userManagement.c
@@ -1,10 +1,49 @@
 #include <stdio.h>
 
+void userRegister(){
+
+}
+void userLogin(){
+
+}
+
+
 void userPrompt(){
-    printf("1.Register\n");
-    printf("2.login\n");
-    printf("3.exit\n");
-    printf("select an option: ");
+    int selectedOption;
+    char selectedOptionChar;
+    while (1)
+    {
+        printf("1.Register\n");
+        printf("2.login\n");
+        printf("3.exit\n");
+        printf("select an option: \n");
+    
+        if (scanf("%i%c", &selectedOption, &selectedOptionChar) != 2 || selectedOptionChar != '\n'){
+            printf("please enter a valid option\n");
+            while(getchar() != '\n');
+            continue;
+        }
+
+        switch (selectedOption)
+        {
+        case 1:
+           userRegister();
+            break;
+        case 2:
+            userLogin();
+            break;
+        case 3:
+            return;
+            break;
+        default:
+            printf("please enter a valid number");
+            break;
+        }
+
+        printf("selected Option: %i\n\n", selectedOption);
+    }
+    
+    
 }
 
 int main(){
