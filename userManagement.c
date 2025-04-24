d7b74ba519f11b5dfef9ce8be50054556ecfa927
SadN00dle <sadnoodle11@gmail.com>
2025-04-24 20:31:03 +0545

    finish without checking for edge cases

--- userManagement.c
+++ userManagement.c
@@ -12,22 +12,67 @@ typedef struct
 User users[MAX_USERS];
 int userCount = 0;
 
+char loginUsername[20];
+char loginPassword[10];
+
 void userRegister(){
+   
     if (userCount > MAX_USERS)
     {
         printf("Users limit reached!");
         return;
     }
     
+    //for username
     printf("Set a userName: ");
     fgets(users[userCount].userName, sizeof(users[userCount].userName), stdin);
-    users[userCount].userName[(strcspn(users[userCount].userName, '\n'))] = '\0 ';
-    printf("username: %s", users[userCount].userName);
+    users[userCount].userName[(strcspn(users[userCount].userName, "\n"))] = '\0';
+    printf("\nusername: %s\n", users[userCount].userName);
+
+    //for password
+    printf("Set a Password: ");
+    fgets(users[userCount].password, sizeof(users[userCount].password), stdin);
+    users[userCount].password[(strcspn(users[userCount].password, "\n"))] = '\0';
+    printf("\nPassword: %s\n", users[userCount].password);
 
     userCount++;
+
+   
 }
 void userLogin(){
+    //for username
+    printf("Enter the userName: ");
+    fgets(loginUsername, sizeof(loginUsername), stdin);
+    loginUsername[strcspn(loginUsername, "\n")] = '\0';
 
+    //for password
+    printf("Enter the Password: ");
+    fgets(loginPassword, sizeof(loginPassword), stdin);
+    loginPassword[strcspn(loginPassword, "\n")] = '\0';
+
+    int isAuthenticated = 0;
+
+    for(int i = 0; i < userCount; i++){
+        if(strcmp(users[i].userName, loginUsername) == 0 &&
+                strcmp( users[i].password, loginPassword) == 0){
+                    isAuthenticated = 1;
+                    break;
+        }
+    }
+    
+    if(isAuthenticated){
+        printf("You have logged in successfully!!\n\n");
+    }else{
+        printf("Wrong username or password\n\n");
+    }
+
+    // if (users[userCount].userName == loginUsername & users[userCount].password == loginPassword)
+    // {
+    //     
+    // }else{
+    //   
+    // }
+    
 }
 
 
@@ -63,11 +108,7 @@ void userPrompt(){
             printf("please enter a valid number");
             break;
         }
-
-        printf("selected Option: %i\n\n", selectedOption);
-    }
-    
-    
+    }   
 }
 
 int main(){
